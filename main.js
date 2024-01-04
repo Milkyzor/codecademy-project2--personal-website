c88f38998e65c2462183845c9c87f213944d0006
Milkyzor <ricardo.rocheta@icloud.com>
2024-01-04 21:00:31 +0000

    html: headerBranchHorizontal (created)
    css:
    .headerBranchVertical (optimised)
    .headerBranchHorizontal (finished)
    
    js:
    openNavBranches() (new features)

--- main.js
+++ main.js
@@ -6,19 +6,20 @@ const nameAndSlogan = document.querySelector('.nameAndSlogan');
 const myName = document.getElementById('name');
 const mySlogan = document.getElementById('slogan');
 
-const headerBranchVertical = document.querySelector('.headerBranchVertical'); // Get the line element
+const headerBranchVertical = document.querySelector('.headerBranchVertical');
+const headerBranchHorizontal = document.querySelector('.headerBranchHorizontal');
 
 
 
 const openNavBranches = () => {
     //nameAndSlogan new style
-    nameAndSlogan.style.height = '110px';
-    nameAndSlogan.style.width = '410px';
+     nameAndSlogan.style.height = '13vh';
+    nameAndSlogan.style.width = '24.5vw';
     nameAndSlogan.style.paddingBottom = '22.5px';
     nameAndSlogan.style.backgroundColor = '#BBE1FA';
     nameAndSlogan.style.color = '#1B262C';
-    myName.style.fontSize = '2.4rem';
-    mySlogan.style.fontSize = '1.25rem';
+    myName.style.fontSize = '4vh';
+    mySlogan.style.fontSize = '2.2vh';
     nameAndSlogan.classList.add('headerClicked'); // Add the 'clicked' class
     nameAndSlogan.style.position = 'absolute';
     nameAndSlogan.style.top = '3vh';
@@ -28,6 +29,7 @@ const openNavBranches = () => {
 
     //branching to navbar
     headerBranchVertical.classList.add('animateHeaderVerticalLine');
+    headerBranchHorizontal.classList.add('animateHeaderHorizontalLine');
 }
 
 nameAndSlogan.addEventListener('click', openNavBranches);
